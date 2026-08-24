a1984a0dcfaad81fe05e0967308ad92ba90e0272
marceloocampo-gdn <marcelo.ocampo@gdn.com.uy>
2026-08-24 15:54:07 -0300

    query

--- sql/Consulta6.sql
+++ sql/Consulta6.sql
@@ -1,4 +1,19 @@
-WITH base AS (
+WITH ARTICULOS_PROPIOS AS (
+    SELECT
+        DISTINCT ARTC_ARTC_ID,
+        ARTC_ARTC_DESC,
+        SC.SUB_NAME,
+        ORIN
+    FROM
+        MSTRDB.DWH.LU_ARTC_ARTICULO AS LAA
+        INNER JOIN MSTRDB.DWH.ITEM_MASTER AS IM ON LAA.ORIN = IM.ITEM
+        INNER JOIN MSTRDB.DWH.CLASS AS CLA ON IM.CLASE = CLA.CLASE
+        INNER JOIN MSTRDB.DWH.SUBCLASS AS SC ON IM.SUBCLASE = SC.SUBCLASE
+    WHERE
+        ARTC_BRAND_ID = 2549
+),
+
+base AS (
     SELECT
         M.SOCI_SOCI_ID,
         V.TICKET,
@@ -8,7 +23,8 @@ WITH base AS (
         C.CLASS_NAME,
         SC.SUB_NAME,
         LAA.ORIN                        AS ARTICULO_ID,
-        LAA.ARTC_ARTC_DESC              AS ARTICULO_DESC
+        LAA.ARTC_ARTC_DESC              AS ARTICULO_DESC,
+        CASE WHEN AP.ARTC_ARTC_ID IS NOT NULL THEN 1 ELSE 0 END AS ES_MARCA_PROPIA
     FROM 
         MSTRDB.DWH.FT_VENTAS V
         INNER JOIN MSTRDB.DWH.FT_FDLN_MOVIMIENTOS M ON V.TICKET = M.TICKET
@@ -18,6 +34,7 @@ WITH base AS (
         INNER JOIN MSTRDB.DWH.CLASS C ON IM.CLASE = C.CLASE
         INNER JOIN MSTRDB.DWH.SUBCLASS SC ON IM.SUBCLASE = SC.SUBCLASE
         INNER JOIN MSTRDB.DWH.LU_GEOG_LOCAL AS L ON V.GEOG_LOCL_ID = L.GEOG_LOCL_ID AND L.GEOG_UNNG_ID = 2
+        LEFT JOIN ARTICULOS_PROPIOS AP ON V.ARTC_ARTC_ID = AP.ARTC_ARTC_ID
     WHERE M.SOCI_SOCI_ID IS NOT NULL
       AND M.FDLN_MOVT_TIPO = 'RP'
       AND (
@@ -130,7 +147,44 @@ SELECT
                                    AND CURRENT_DATE - 1
             THEN b.SOCI_SOCI_ID END)
         / NULLIF(t.TOTAL_SOCIOS_ACTUAL, 0) * 100
-    , 1)                                AS PCT_PENETRACION_ACTUAL
+    , 1)                                AS PCT_PENETRACION_ACTUAL,
+
+    -- === MARCA PROPIA (dentro de la apertura) ===
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                               AND CURRENT_DATE - 1
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                               AND DATEADD('year', -1, CURRENT_DATE - 1)
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_AA,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_ACTUAL,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_AA
 
 FROM base b
 CROSS JOIN total_cadena t
@@ -223,7 +277,43 @@ SELECT
                                    AND CURRENT_DATE - 1
             THEN b.SOCI_SOCI_ID END)
         / NULLIF(t.TOTAL_SOCIOS_ACTUAL, 0) * 100
-    , 1)                                AS PCT_PENETRACION_ACTUAL
+    , 1)                                AS PCT_PENETRACION_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                               AND CURRENT_DATE - 1
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                               AND DATEADD('year', -1, CURRENT_DATE - 1)
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_AA,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_ACTUAL,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_AA
 
 FROM base b
 CROSS JOIN total_cadena t
@@ -316,7 +406,43 @@ SELECT
                                    AND CURRENT_DATE - 1
             THEN b.SOCI_SOCI_ID END)
         / NULLIF(t.TOTAL_SOCIOS_ACTUAL, 0) * 100
-    , 1)                                AS PCT_PENETRACION_ACTUAL
+    , 1)                                AS PCT_PENETRACION_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                               AND CURRENT_DATE - 1
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                               AND DATEADD('year', -1, CURRENT_DATE - 1)
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_AA,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_ACTUAL,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_AA
 
 FROM base b
 CROSS JOIN total_cadena t
@@ -324,7 +450,7 @@ GROUP BY b.DEPT_NAME, b.CLASS_NAME, b.SUB_NAME, t.TOTAL_SOCIOS_ACTUAL, t.TOTAL_S
 
 UNION ALL
 
--- NIVEL ARTICULO  (nuevo)
+-- NIVEL ARTICULO
 SELECT
     'ARTICULO'                          AS NIVEL,
     b.DEPT_NAME                         AS DEPARTAMENTO,
@@ -403,14 +529,49 @@ SELECT
             THEN b.VNTA_IMPORTE_SIN_IVA ELSE 0 END), 0) * 100
     , 1)                                AS VAR_MONTO_PCT,
 
-    -- Penetracion del articulo sobre el TOTAL de la cadena
     ROUND(
         COUNT(DISTINCT CASE
             WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
                                    AND CURRENT_DATE - 1
             THEN b.SOCI_SOCI_ID END)
         / NULLIF(t.TOTAL_SOCIOS_ACTUAL, 0) * 100
-    , 1)                                AS PCT_PENETRACION_ACTUAL
+    , 1)                                AS PCT_PENETRACION_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                               AND CURRENT_DATE - 1
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_ACTUAL,
+
+    COUNT(DISTINCT CASE
+        WHEN b.ES_MARCA_PROPIA = 1
+         AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                               AND DATEADD('year', -1, CURRENT_DATE - 1)
+        THEN b.SOCI_SOCI_ID END)        AS SOCIOS_UNICOS_MP_AA,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, CURRENT_DATE - 1)
+                                   AND CURRENT_DATE - 1
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_ACTUAL,
+
+    ROUND(
+        COUNT(DISTINCT CASE
+            WHEN b.ES_MARCA_PROPIA = 1
+             AND b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END)
+        / NULLIF(COUNT(DISTINCT CASE
+            WHEN b.TIEM_DIA_ID BETWEEN DATEADD('day', -30, DATEADD('year', -1, CURRENT_DATE - 1))
+                                   AND DATEADD('year', -1, CURRENT_DATE - 1)
+            THEN b.SOCI_SOCI_ID END), 0) * 100
+    , 1)                                AS PCT_PENETRACION_MP_AA
 
 FROM base b
 CROSS JOIN total_cadena t
